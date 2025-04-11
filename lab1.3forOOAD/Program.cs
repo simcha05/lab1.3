c9779d6d8768951a5011a61d309831ba8ffd1c35
Pro <pro@MacBook-Pro-Pro.local>
2025-04-12 00:06:35 +0300

    Add new file

---
+++ lab1.3forOOAD/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 5, 3, 8, 1, 2, 7, 4, 6 };
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        Array.Sort(array);
+
+        Console.WriteLine("\nОтсортированный массив:");
+        PrintArray(array);
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
+
